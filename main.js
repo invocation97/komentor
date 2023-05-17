7c98bc0c7f75057ab046cded5bbf6766e03531b7
invocation97 <danilo97benovic@gmail.com>
2023-05-18 00:31:17 +0200

    Test

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const learningContentTextarea = document.getElementById('learningContent');
-const teachersFeedbackTextarea = document.getElementById('teachersFeedback');
+const learningContentTextarea = document.getElementById('learningContent')
+const teachersFeedbackTextarea = document.getElementById('teachersFeedback')
 
-document.getElementById('learningContentSave').addEventListener('click', saveLearningContent);
-document.getElementById('teachersFeedbackSave').addEventListener('click', saveTeachersFeedback);
+learningContentTextarea.addEventListener('click', saveLearningContent)
+teachersFeedbackTextarea.addEventListener('click', saveTeachersFeedback)
 
 window.addEventListener('load', loadSavedContents);
 
-function saveLearningContent() {
-  const learningContent = learningContentTextarea.value;
-  sessionStorage.setItem('learningContent', learningContent);
+function saveLearningContent(){
+    const learningContent = learningContentTextarea.value
+    localStorage.setItem('learningContent', learningContent)
 }
 
 function saveTeachersFeedback() {
-  const teachersFeedback = teachersFeedbackTextarea.value;
-  sessionStorage.setItem('teachersFeedback', teachersFeedback);
-}
+    const teachersFeedback = teachersFeedbackTextarea.value;
+    localStorage.setItem('teachersFeedback', teachersFeedback);
+  }
 
 function loadSavedContents() {
-  const savedLearningContent = sessionStorage.getItem('learningContent');
-  const savedTeachersFeedback = sessionStorage.getItem('teachersFeedback');
+const savedLearningContent = localStorage.getItem('learningContent');
+const savedTeachersFeedback = localStorage.getItem('teachersFeedback');
 
-  if (savedLearningContent) {
+if (savedLearningContent) {
     learningContentTextarea.value = savedLearningContent;
-  }
+}
 
-  if (savedTeachersFeedback) {
+if (savedTeachersFeedback) {
     teachersFeedbackTextarea.value = savedTeachersFeedback;
-  }
+}
 }
