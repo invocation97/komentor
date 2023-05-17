a4958cb729d204e4fc9b3b30903c05864f96f9a8
invocation97 <danilo97benovic@gmail.com>
2023-05-18 01:42:46 +0200

    firebase

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 console.log("main.js loaded");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-window.addEventListener("load", function() {
-  alert("main.js has been loaded!");
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWgUrK4rO1spfMTBqytgsfXFitcRd0P-A",
+  authDomain: "komentor-6cb74.firebaseapp.com",
+  projectId: "komentor-6cb74",
+  storageBucket: "komentor-6cb74.appspot.com",
+  messagingSenderId: "1018810922529",
+  appId: "1:1018810922529:web:4d00570460bfb22edcd560",
+  measurementId: "G-CB6F1SJP9B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+var db = firebase.firestore();
+
+// Example: Save data to Firestore
+function saveData() {
+  var learningContent = document.getElementById("learningContent").value;
+  var teachersFeedback = document.getElementById("teachersFeedback").value;
+
+  db.collection("comments").add({
+    learningContent: learningContent,
+    teachersFeedback: teachersFeedback
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
+
+
 
 const learningContentTextarea = document.getElementById('learningContent')
 const teachersFeedbackTextarea = document.getElementById('teachersFeedback')
